86e8dfa5ae42e69fb1ef81a107b07e827367fab7
arun <jkarun63@gmail.com>
2018-02-20 08:46:48 +0530

    project commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RemoteServiceProvider } from '../../providers/remote-service/remote-service';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  users: any; 
+  constructor(public navCtrl: NavController, private remoteService : RemoteServiceProvider ) {
+    this.getUsers();
   }
-
+  
+    getUsers() {
+    this.remoteService.getUsers()
+    .then(data => {
+      this.users = data;
+      console.log(this.users);
+    });
+  }
 }
